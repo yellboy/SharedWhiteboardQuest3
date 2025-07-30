016d8be4110b273db314442278cbfcc98afe50ac
bojan.jelaca@zuehlke.com <Bojan.Jelaca@zuehlke.com>
2025-07-30 12:13:31 +0200

    host

--- SharedWhiteboardAPI/Program.cs
+++ SharedWhiteboardAPI/Program.cs
@@ -2,6 +2,21 @@ using SharedWhiteboardAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Kestrel to listen on all network interfaces (0.0.0.0)
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(System.Net.IPAddress.Any, 5100); // Listen on port 5100 for HTTP
+});
+builder.Services.Configure<Microsoft.AspNetCore.Server.Kestrel.Core.KestrelServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+builder.Services.Configure<Microsoft.AspNetCore.HostFiltering.HostFilteringOptions>(options =>
+{
+    options.AllowedHosts = new List<string> { "localhost", "192.168.1.71", "127.0.0.1", "*" };
+});
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -30,10 +45,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers(); 
+
+app.UseCors(builder =>
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 
 app.Run();
